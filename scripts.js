4c73b4677338cedeea13ade0f4e77fc0a03e4677
Lelio Victor <leliov.vieira@gmail.com>
2022-04-07 21:56:37 -0300

    Adicionado a logica, classes e funcoes, que rotacionará a carta

---
+++ scripts.js
@@ -0,0 +1,5 @@
+function selected(element) {
+    element.querySelector(".front-card").classList.add("rotation_front-face");
+    element.querySelector(".back-card").classList.add("rotation_back-face");
+
+}
