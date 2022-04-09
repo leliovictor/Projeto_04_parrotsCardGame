35f203c20480d0e9bf24ad8263c1187febe31f78
Lelio Victor <leliov.vieira@gmail.com>
2022-04-09 12:37:00 -0300

    Feito o inicio do jogo, para inserir o numero de cartas e a checagem se é um numero valido

--- scripts.js
+++ scripts.js
@@ -1,5 +1,33 @@
+let numberCards, selectedItemsCheck = [], countClicks = 0;
+
+function startGame() {
+  numberCards = prompt("Com quantas cartas deseja jogar? Insira um numero par, entre 4 e 14.");
+
+  while (numberCards < 4 || numberCards > 14 || numberCards % 2 != 0) {
+    alert("Número inválido, insira um número par entre 4 e 14.");
+    numberCards = prompt("Com quantas cartas deseja jogar? Insira um numero par, entre 4 e 14.");
+  }
+}
+
 function selected(element) {
+    flipCard(element);
+
+    registerCard(element);
+
+}
+
+function flipCard(element) {
+  if (element.querySelector(".rotation_front-face") === null) {
     element.querySelector(".front-card").classList.add("rotation_front-face");
     element.querySelector(".back-card").classList.add("rotation_back-face");
 
+    countClicks++;
+  }
 }
+
+function registerCard(element) {
+
+}
+
+
+startGame();
